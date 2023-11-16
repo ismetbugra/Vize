b4aa197b5d307df9fa819d2737b8d39624a55e18
ismetbugra <ismetbugri2002@gmail.com>
2023-11-16 19:25:06 +0300

    1.2 testleri ile tamamlanmistir

---
+++ 210401053/Labex1_Q1_210401053_ismetbugra_topyurt.R
@@ -0,0 +1,5 @@
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+file_name <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = file_name,mode="wb")
+
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv", sep = ";", header = TRUE)
