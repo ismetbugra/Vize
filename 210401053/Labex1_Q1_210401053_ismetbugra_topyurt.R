1d45471b5447a2226e6f6e7f4ecab97f78aefc1c
ismetbugra <ismetbugri2002@gmail.com>
2023-11-16 19:58:07 +0300

    210401053_1_7
    1.7 testleri ile tamamlanmistir

---
+++ 210401053/Labex1_Q1_210401053_ismetbugra_topyurt.R
@@ -0,0 +1,20 @@
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+file_name <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = file_name,mode="wb")
+
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv", sep = ";", header = TRUE)
+
+maps$Latitude <- gsub("N", "", maps$Latitude)
+
+idx <- grep("W", maps$Longitude)
+
+maps$Longitude <- gsub("[EW]", "", maps$Longitude)
+
+maps$Year <- gsub("AD", "", maps$Year)
+
+maps$Latitude <- as.numeric(gsub("[NW]", "", maps$Latitude))
+maps$Longitude <- as.numeric(gsub("[EW]", "", maps$Longitude))
+
+maps$Year <- as.numeric(gsub("AD", "", maps$Year))
+
+hist(maps$Year, breaks = 10, main = "Histogram of Years", xlab = "Year", ylab = "Frequency", col = "red", border = "black")
