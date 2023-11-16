f4d49a15d01a63d8afccc345dbf28186475fe285
ismetbugra <ismetbugri2002@gmail.com>
2023-11-16 19:49:25 +0300

    210401053_1_6
    1.6 testleri ile tamamlanmistir

---
+++ 210401053/Labex1_Q1_210401053_ismetbugra_topyurt.R
@@ -0,0 +1,13 @@
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+file_name <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = file_name,mode="wb")
+
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv", sep = ";", header = TRUE)
+
+maps$Latitude <- gsub("N", "", maps$Latitude)
+
+idx <- grep("W", maps$Longitude)
+
+maps$Longitude <- gsub("[EW]", "", maps$Longitude)
+
+maps$Year <- gsub("AD", "", maps$Year)
